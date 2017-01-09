d377430c3ec131bc440ce5ee1108ce1fabda06e2
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-01-09 10:50:38 +0800

    Created death zone, coins, victory, particle effects, enemy (attacks, moves and stuns)

--- coursera/game development for modern platforms/Assets/Scripts/EnemyStun.cs
+++ coursera/game development for modern platforms/Assets/Scripts/EnemyStun.cs
@@ -10,6 +10,9 @@ public class EnemyStun : MonoBehaviour {
 		{
 			// tell the enemy to be stunned
 			this.GetComponentInParent<Enemy>().Stunned();
-		}
+
+            // Make the player bounce the enemy (aka, jump)
+            other.gameObject.GetComponent<CharacterController2D>().EnemyBounce();
+        }
 	}
 }
