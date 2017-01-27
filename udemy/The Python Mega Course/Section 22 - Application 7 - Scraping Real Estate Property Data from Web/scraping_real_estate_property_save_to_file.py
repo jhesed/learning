2fb74181738c061f794b255fd473e8087642b18a
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-01-27 18:54:35 +0800

    (Beautiful Soup) Added script for scraping multiple pages and saving results to csv file

--- udemy/The Python Mega Course/Section 22 - Application 7 - Scraping Real Estate Property Data from Web/scraping_real_estate_property_save_to_file.py
+++ udemy/The Python Mega Course/Section 22 - Application 7 - Scraping Real Estate Property Data from Web/scraping_real_estate_property_save_to_file.py
@@ -31,83 +31,97 @@ import pandas
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-
+    BASE_URL = 'http://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/t=0&s='
 
     # ... SECTIONS :: 
     # 142: Demonstration of Webscraping Application
     # 143: Understanding the Problem and Loading the Webpage in Python
     # 144: Extracting Divisions of All Properties
 
-    # Load the first page
+    # Get page count
+
     req = requests.get('http://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/')
     content = req.content
 
     soup = BeautifulSoup(content, 'html.parser')
+    # print(soup.prettify())
+    page_count = int(soup.find_all('a', {'class': 'Page'})[-1].text)
 
-    # Get all real estate proerty rows
-    all_properties = soup.find_all('div', {'class': 'propertyRow'})
-
-    # ... SECTIONS ::
-    # 145: Extracting Addresses and Property Details
-    
-    # List to hold the dictionary of values
+    # List to hold the dictionary of results
     l = []
-    for item in all_properties:
+
+    # Crawls in pages, step is 10 as observed in the url
         
-        # Dict to hold the item values
-        d = {}
+    for page in range(0, page_count*10, 10):
 
-        # Retrieve the address
-        d['address'] = item.find_all('span', 'propAddressCollapse')[0].text
-        d['locality'] = item.find_all('span', 'propAddressCollapse')[1].text
+        # Load the page dynamically
+        req = requests.get(BASE_URL + str(page) + '.html')
+        content = req.content
 
-        # Retrieve the prices
-        d['price'] = item.find('h4', {'class', 'propPrice'}).text.strip()
-                
-        try:
-            # Retrieve only the number of beds
-            d['bed_count'] = item.find('span', {'class': 'infoBed'}).find('b').text
-           
-        except:
-            d['bed_count'] = None
+        soup = BeautifulSoup(content, 'html.parser')
 
-        try:
-            # Retrieve square feet
-            d['sqft'] = item.find('span', {'class': 'infoSqFt'}).find('b').text
-        except:
-            d['sqft'] = None
-        
-        try:
-            # Retrieve only the number of full bath
-            d['full_bath_count'] = item.find(
-                'span', {'class': 'infoValueFullBath'}).find('b').text
-        except:
-            d['full_bath_count'] = None
-
-        try:
-            d['half_bath_count'] = item.find(
-                'span', {'class': 'infoValueHalfBath'}).find('b').text
-            
-        except:
-            d['half_bath_count'] = None
+        # Get all real estate proerty rows
+        all_properties = soup.find_all('div', {'class': 'propertyRow'})
 
         # ... SECTIONS ::
-        # 146: Extracting Elements with no Unique Identifiers
+        # 145: Extracting Addresses and Property Details
+     
+        for item in all_properties:
+            
+            # Dict to hold the item values
+            d = {}
 
-        for column_group in item.find_all('div', {'class': 'columnGroup'}):
-            # print(column_group)
+            # Retrieve the address
+            d['address'] = item.find_all('span', 'propAddressCollapse')[0].text
+            d['locality'] = item.find_all('span', 'propAddressCollapse')[1].text
 
-            for feature_group, feature_name in zip(
-                column_group.find_all('span', {'class': 'featureGroup'}),
-                column_group.find_all('span', {'class': 'featureName'})):
+            # Retrieve the prices
+            d['price'] = item.find('h4', {'class', 'propPrice'}).text.strip()
+                    
+            try:
+                # Retrieve only the number of beds
+                d['bed_count'] = item.find('span', {'class': 'infoBed'}).find('b').text
+               
+            except:
+                d['bed_count'] = None
 
-                if 'lot size' in feature_group.text.lower():
-                    d['lot_size'] = feature_name.text
-                else:
-                    d['lot_size'] = None
+            try:
+                # Retrieve square feet
+                d['sqft'] = item.find('span', {'class': 'infoSqFt'}).find('b').text
+            except:
+                d['sqft'] = None
+            
+            try:
+                # Retrieve only the number of full bath
+                d['full_bath_count'] = item.find(
+                    'span', {'class': 'infoValueFullBath'}).find('b').text
+            except:
+                d['full_bath_count'] = None
+
+            try:
+                d['half_bath_count'] = item.find(
+                    'span', {'class': 'infoValueHalfBath'}).find('b').text
                 
-        # Append to list
-        l.append(d)
+            except:
+                d['half_bath_count'] = None
+
+            # ... SECTIONS ::
+            # 146: Extracting Elements with no Unique Identifiers
+
+            for column_group in item.find_all('div', {'class': 'columnGroup'}):
+                # print(column_group)
+
+                for feature_group, feature_name in zip(
+                    column_group.find_all('span', {'class': 'featureGroup'}),
+                    column_group.find_all('span', {'class': 'featureName'})):
+
+                    if 'lot size' in feature_group.text.lower():
+                        d['lot_size'] = feature_name.text
+                    else:
+                        d['lot_size'] = None
+                    
+            # Append to list
+            l.append(d)
 
     # Create a Pandas Data Frame using the list
     df = pandas.DataFrame(l)
