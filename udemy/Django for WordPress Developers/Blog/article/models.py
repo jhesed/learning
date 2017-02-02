7414374a5bb3323aec2e3051a5ea7160930d26fb
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-02-02 11:30:04 +0800

    (Django) Added additional fields in Article model. Set the Article title to show on the admin page

--- udemy/Django for WordPress Developers/Blog/article/models.py
+++ udemy/Django for WordPress Developers/Blog/article/models.py
@@ -7,10 +7,21 @@ Date: 2017-02-02
 from django.db import models
 from django.utils import timezone
 
+# -----------------------------------------------------------------------------
 class Post(models.Model):
     """
     Defines the Post model class
     """
 
-    author = models.ForeignKey('auth.User') 
+    author = models.ForeignKey('auth.User')  # Pulls user names from User table
+    title = models.CharField(max_length=150, null=True)
+    text = models.TextField(null=True)
+    created_date = models.DateTimeField(default=timezone.now)
 
+    # -------------------------------------------------------------------------
+    def __str__(self):
+        """
+        This will allow Django admin to return the title instead
+        of the "Post object" string
+        """
+        return self.title
