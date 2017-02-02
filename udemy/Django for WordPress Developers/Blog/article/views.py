b0174f399f01112bc7edcf0cb345b6aa2143c4c5
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-02-02 11:48:29 +0800

    Removed .pyc files as these are not needed in a github repository

--- udemy/Django for WordPress Developers/Blog/article/views.py
+++ udemy/Django for WordPress Developers/Blog/article/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    """
+    `templates` folder should be manually created within the application
+    It will automatically load the htmls from this folder
+    """
+    return render(request, 'pages/home.html')
