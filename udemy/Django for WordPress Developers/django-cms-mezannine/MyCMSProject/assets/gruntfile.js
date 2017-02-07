351de4360f718f917b95c243fe516f05f2aa821c
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-02-07 14:27:55 +0800

    (Django) Added grunt for automated creation of css based from scss file

---
+++ udemy/Django for WordPress Developers/django-cms-mezannine/MyCMSProject/assets/gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    '../static/css/style.css' : 'sass/style.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['sass/style.scss'],
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+}
