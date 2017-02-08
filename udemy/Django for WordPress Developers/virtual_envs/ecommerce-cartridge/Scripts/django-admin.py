be372277849b6614249fda18674fdea6b5f30b3e
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-02-08 13:21:52 +0800

    (Django Salesor) Added virtual env files

---
+++ udemy/Django for WordPress Developers/virtual_envs/ecommerce-cartridge/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jhese154\docs\learning\github\udemy\django~1\virtua~1\ecomme~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
